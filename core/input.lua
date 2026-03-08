12a26b22efdcd3df94114937bb6372f5ef9a1168
Halim Zaghdoudi <halim.zag@gmx.de>
2026-03-08 19:41:28 +0000

    Create input.lua

---
+++ core/input.lua
@@ -0,0 +1,15 @@
+-- core/input.lua
+INPUT = {}
+
+-- Maps pixel (x, y) to grid index (1-based)
+function INPUT.ToIdx(pixelX, pixelY, cellSize)
+    local gridX = math.floor(pixelX / cellSize) + 1
+    local gridY = math.floor(pixelY / cellSize) + 1
+
+    -- OOB Check (Bounds Guard)
+    if gridX < 1 or gridX > GRID_SIZE or gridY < 1 or gridY > GRID_SIZE then
+        return nil
+    end
+
+    return (gridY - 1) * GRID_SIZE + gridX
+end
