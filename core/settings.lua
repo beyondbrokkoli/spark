d9bebecd7c46fdf281e7deae183e1e47dc599869
Halim Zaghdoudi <halim.zag@gmx.de>
2026-03-09 10:00:46 +0000

    Create settings.lua

---
+++ core/settings.lua
@@ -0,0 +1,18 @@
+-- core/settings.lua
+SETTINGS = {
+    -- World Geometry
+    GRID_BUFFER_SIZE = 10240,
+    CELL_SIZE = 5,
+
+    -- Camera Movement
+    CAMERA_SPEED = 10000, -- Pixels per second
+
+    -- Controls
+    KEYS = {
+        UP    = "w",
+        DOWN  = "s",
+        LEFT  = "a",
+        RIGHT = "d",
+        ERASE = "lshift"
+    }
+}
